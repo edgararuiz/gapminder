ee015846317cfb7e9ac485c9c0b358283751d95d
Edgar Ruiz <edgararuiz@gmail.com>
2018-09-04 17:41:24 -0500

    switch to on_attch()

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
 .onAttach <- function(...) {
-  datalang::load_package_translations(
-    spec_folder =  system.file("translations", package = "gapminder"),
-    envir = as.environment("package:gapminder")
-  )
+  datalang::on_attach("gapminder")
 }
